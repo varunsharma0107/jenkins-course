9dc557ebbe16dbf0e92bf44d3cb280431a0cf049
varunsharma0107 <49973284+varunsharma0107@users.noreply.github.com>
2019-06-25 21:36:21 +0530

    updated my GitHub email and user

--- job-dsl/nodejs2.groovy
+++ job-dsl/nodejs2.groovy
@@ -1,8 +1,8 @@
-job('NodeJS example') {
+job('NodeJS-example2') {
     scm {
         git('git://github.com/varunsharma0107/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName('DSL User - Varun')
+            node / gitConfigEmail('er.varunsharma1357@gmail.com')
         }
     }
     triggers {
